df1c5391ce0dd16cfaf6540ac971b355af9775ed
yunjung <yunjj21@hs.ac.kr>
2022-04-03 15:18:03 +0900

    step 9 finish
    step 9 finish

--- app/src/main/java/com/yunjung/composelab/MainActivity.kt
+++ app/src/main/java/com/yunjung/composelab/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -56,11 +58,11 @@ private fun OnBoardingScreen(onContinueClicked : () -> Unit){
 }
 
 @Composable
-private fun Greetings(names : List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(4.dp)) {
-        for(name in names){
+private fun Greetings(names : List<String> = List(100){"$it"}) {
+    LazyColumn(modifier = Modifier.padding(4.dp)) {
+        items(items = names) { name ->
             Greeting(name = name)
-        }       
+        }
     }
 }
 
