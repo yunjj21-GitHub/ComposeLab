2da91c807830027f51f1f371820e8ee2869c9729
yunjung <yunjj21@hs.ac.kr>
2022-04-03 15:05:02 +0900

    step 8 finish
    step 8 finish

--- app/src/main/java/com/yunjung/composelab/MainActivity.kt
+++ app/src/main/java/com/yunjung/composelab/MainActivity.kt
@@ -3,17 +3,10 @@ package com.yunjung.composelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,43 +24,86 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MyApp(names : List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names){
-            Greeting(name)
+fun MyApp(){
+    var shouldShowOnBoarding by remember{
+        mutableStateOf(true)
+    }
+
+    if(shouldShowOnBoarding){
+        OnBoardingScreen(onContinueClicked = {shouldShowOnBoarding = false})
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+private fun OnBoardingScreen(onContinueClicked : () -> Unit){
+    Surface{
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ){
+                Text("Continue")
+            }
         }
     }
 }
 
 @Composable
-private fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false)} // 확장 되었는지 여부를 저장
+private fun Greetings(names : List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(4.dp)) {
+        for(name in names){
+            Greeting(name = name)
+        }       
+    }
+}
 
+@Composable
+private fun Greeting(name : String) {
+    val expanded = remember { mutableStateOf(false)}
+    
     val extraPadding = if(expanded.value) 48.dp else 0.dp
-
-    Surface(color = MaterialTheme.colors.primary,
-    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+    
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ){
         Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier
                 .weight(1f)
                 .padding(bottom = extraPadding)
-            ) {
-                Text(text = "Hello,")
+            ){
+                Text(text = "Hello, ")
                 Text(text = name)
             }
             OutlinedButton(
                 onClick = { expanded.value = !expanded.value }
             ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+                Text(text = if(expanded.value) "Show less" else "Show more")
             }
         }
     }
 }
 
+/* 화면 미리보기 */
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     ComposeLabTheme {
-        MyApp()
+        Greetings()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnBoardingPreview(){
+    ComposeLabTheme{
+        OnBoardingScreen(onContinueClicked = {})
     }
 }
